d3bd9a742e6bb52b10a021ac15ad8101092f9671
othmanmtsm <motassimothman@gmail.com>
2021-03-30 20:52:39 +0000

    tried to fix uninitialized memory issue

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "hblk_crypto.h"
 
 /**
@@ -10,7 +7,6 @@
  */
 EC_KEY *ec_create(void)
 {
-	uint8_t gen_key_status;
 	EC_KEY *key;
 
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
@@ -18,15 +14,11 @@ EC_KEY *ec_create(void)
 	{
 		return (NULL);
 	}
-	else
+	if (!EC_KEY_generate_key(key))
 	{
-		gen_key_status = EC_KEY_generate_key(key);
-		if (gen_key_status != 1)
-		{
-			fprintf(stderr, "%s", "ec_create.c : error while generating the key\n");
-			EC_KEY_free(key);
-			return (NULL);
-		}
+		fprintf(stderr, "%s", "ec_create.c : error while generating the key\n");
+		EC_KEY_free(key);
+		return (NULL);
 	}
 	return (key);
 }
