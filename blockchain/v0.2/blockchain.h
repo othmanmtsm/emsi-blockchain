3e73f087b1d13433ea3efd8e179958f9d248a5e1
othmanmtsm <motassimothman@gmail.com>
2021-05-02 13:34:14 +0000

    v0.2 Task 2

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -153,6 +153,7 @@ int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
 int block_is_valid(block_t const *block, block_t const *prev_block);
 int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
+void block_mine(block_t *block);
 
 #endif
 
