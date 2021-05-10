3477b2e82482541c93b128e617de8e27a0c0a8d7
othmanmtsm <motassimothman@gmail.com>
2021-05-10 11:54:25 +0000

    edit blockchain create

--- blockchain/v0.1/block_create.c
+++ blockchain/v0.1/block_create.c
@@ -11,22 +11,24 @@
 block_t *block_create(block_t const *prev, int8_t const *data,
 			uint32_t data_len)
 {
-	block_t *block;
+	block_t *block = NULL;
 
-	block = malloc(sizeof(*block));
+	if (!prev || !data)
+	{
+		return (NULL);
+	}
+	block = calloc(1, sizeof(*block));
+	if (!block)
+	{
+		return (NULL);
+	}
 	block->info.index = prev->info.index + 1;
 	block->info.difficulty = 0;
 	block->info.timestamp = time(NULL);
 	block->info.nonce = 0;
-	memcpy(&(block->info.prev_hash), prev->hash, SHA256_DIGEST_LENGTH);
-
-	if (data_len > BLOCKCHAIN_DATA_MAX)
-		data_len = BLOCKCHAIN_DATA_MAX;
-
-	memcpy(&(block->data.buffer), data, data_len);
-	block->data.len = data_len;
-	memset(block->hash, 0, SHA256_DIGEST_LENGTH);
-
+	memcpy(block->info.prev_hash, prev->hash, SHA256_DIGEST_LENGTH);
+	block->data.len = MIN(data_len, BLOCK_DATA_MAX_LEN);
+	memcpy(block->data.buffer, data, block->data.len);
 	return (block);
 }
 
--- blockchain/v0.1/blockchain_create.c
+++ blockchain/v0.1/blockchain_create.c
@@ -8,22 +8,15 @@
 
 blockchain_t *blockchain_create(void)
 {
-	blockchain_t *blockchain;
-	block_t *block;
+	blockchain_t *blockchain = calloc(1, sizeof(*blockchain));
+	block_t *block = calloc(1, sizeof(*block));
 
-	blockchain = malloc(sizeof(*blockchain));
-	block = malloc(sizeof(*block));
-	block->info.index = 0;
-	block->info.difficulty = 0;
-	block->info.timestamp = 1537578000;
-	block->info.nonce = 0;
-	memset(block->info.prev_hash, 0, SHA256_DIGEST_LENGTH);
-	memcpy(&(block->data.buffer), "Holberton School", 16);
-	block->data.len = 16;
-	memcpy(&(block->hash), "\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf"
-				"\x2a\x97\xd4\x8d\x0c\x8e\x00\x09\xc8\x17\xf2\xb1"
-				"\xd3\xd7\xff\x2f\x04\x51\x58\x03",
-		SHA256_DIGEST_LENGTH);
+	if (!blockchain || !block)
+	{
+		free(blockchain);
+		free(block);
+		return (NULL);
+	}
 	blockchain->chain = llist_create(MT_SUPPORT_FALSE);
 	if (!blockchain->chain)
 	{
@@ -31,8 +24,23 @@ blockchain_t *blockchain_create(void)
 		free(block);
 		return (NULL);
 	}
+	block->info.index = BLOCK_GENESIS_INIT_INFO_INDEX;
+	block->info.difficulty = BLOCK_GENESIS_INIT_INFO_DIFFICULTY;
+	block->info.timestamp = BLOCK_GENESIS_INIT_INFO_TIMESTAMP;
+	block->info.nonce = BLOCK_GENESIS_INIT_INFO_NONCE;
+	memcpy(
+		block->info.prev_hash,
+		BLOCK_GENESIS_INIT_INFO_PREV_HASH, SHA256_DIGEST_LENGTH);
+	memcpy(
+		block->data.buffer,
+		BLOCK_GENESIS_INIT_DATA_BUFFER, BLOCK_GENESIS_INIT_DATA_LEN);
+	block->data.len = BLOCK_GENESIS_INIT_DATA_LEN;
+	memcpy(
+		block->hash,
+		BLOCK_GENESIS_INIT_HASH, SHA256_DIGEST_LENGTH);
 	if (llist_add_node(blockchain->chain, block, ADD_NODE_FRONT) == -1)
 	{
+		llist_destroy(blockchain->chain, 1, NULL);
 		free(blockchain);
 		free(block);
 		return (NULL);
